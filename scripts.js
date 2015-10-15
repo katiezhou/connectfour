368c7e5ccef65a546a9479e2bed092596173dd15
Katie Zhou <katiezhou@Katies-MBP.zillionz.net>
2015-10-14 23:03:58 -0400

    populating row, column, and diagonal arrays in a different way, beginning function to place gamepiece in lowest spot

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@
 // 																		Connect Four 
 // ===================================================================================
 
-// ============================
-// 			Global Variables
-// ============================
+// ==============================================
+// 			Global Variables + Populating Arrays
+// ==============================================
 var gameboard = [null, null, null, null, null, null, null, 
                  null, null, null, null, null, null, null, 
                  null, null, null, null, null, null, null, 
@@ -12,101 +12,52 @@ var gameboard = [null, null, null, null, null, null, null,
                  null, null, null, null, null, null, null, 
                  null, null, null, null, null, null, null]
 
-// I'm creating one array each for rows, columns, and diagonals with more than 4 spaces to simplify
+// I'm creating one array for each row, column, and diagonal with more than 4 spaces to simplify
 // the function that checks for a win later on. These variables just grab elements with the corresponding
 // classes from the HTML doc.
-var $rows = [[], [], [], [], [], []];
-var $cols = [[], [], [], [], [], [], []];
-var $diags = [[], [], [], [], [], [], [], [], [], [], [], []];
-
-var $row0 = $(".row0");
-var $row1 = $(".row1");
-var $row2 = $(".row2");
-var $row3 = $(".row3");
-var $row4 = $(".row4");
-var $row5 = $(".row5");
-
-var $col0 = $(".col0");
-var $col1 = $(".col1");
-var $col2 = $(".col2");
-var $col3 = $(".col3");
-var $col4 = $(".col4");
-var $col5 = $(".col5");
-var $col6 = $(".col6");
-
-var $diag0 = $(".diag0");
-var $diag1 = $(".diag1");
-var $diag2 = $(".diag2");
-var $diag3 = $(".diag3");
-var $diag4 = $(".diag4");
-var $diag5 = $(".diag5");
-var $diag6 = $(".diag6");
-var $diag7 = $(".diag7");
-var $diag8 = $(".diag8");
-var $diag9 = $(".diag9");
-var $diag10 = $(".diag10");
-var $diag11 = $(".diag11");
-
-
-for (var i = 0; i < 7; i++) {
-	$rows[0].push($row0[i]);
-	$rows[1].push($row1[i]);
-	$rows[2].push($row2[i]);
-	$rows[3].push($row3[i]);
-	$rows[4].push($row4[i]);
-	$rows[5].push($row5[i]);
-}
-
-for (var i = 0; i < 6; i++) {
-	$cols[0].push($col0[i]);
-	$cols[1].push($col1[i]);
-	$cols[2].push($col2[i]);
-	$cols[3].push($col3[i]);
-	$cols[4].push($col4[i]);
-	$cols[5].push($col5[i]);
-	$cols[6].push($col6[i]);
-}
 
 
 
-for (var i = 0; i < 4; i++) {
-	$diags[0].push($diag0[i]);
-}
-for (var i = 0; i < 5; i++) {
-	$diags[1].push($diag1[i]);
-}
-for (var i = 0; i < 6; i++) {		
-	$diags[2].push($diag2[i]);
-}
-for (var i = 0; i < 6; i++) {		
-	$diags[3].push($diag3[i]);
-}
-for (var i = 0; i < 5; i++) {		
-	$diags[4].push($diag4[i]);
-}
-for (var i = 0; i < 4; i++) {		
-	$diags[5].push($diag5[i]);
-}
-for (var i = 0; i < 4; i++) {		
-	$diags[6].push($diag6[i]);
-}
-for (var i = 0; i < 5; i++) {		
-	$diags[7].push($diag7[i]);
-}
-for (var i = 0; i < 6; i++) {		
-	$diags[8].push($diag8[i]);
-}
-for (var i = 0; i < 6; i++) {		
-	$diags[9].push($diag9[i]);
-}
-for (var i = 0; i < 5; i++) {		
-	$diags[10].push($diag10[i]);
-}
-for (var i = 0; i < 4; i++) {		
-	$diags[11].push($diag11[i]);
-}
 
+// var $rows = [[], [], [], [], [], []];
+// var $cols = [[], [], [], [], [], [], []];
+// var $diags = [[], [], [], [], [], [], [], [], [], [], [], []];
 
+var $columns = $(".column");
+
+var $col0 = jQuery.makeArray($($("#col0")).children());
+var $col1 = jQuery.makeArray($($("#col1")).children());
+var $col2 = jQuery.makeArray($($("#col2")).children());
+var $col3 = jQuery.makeArray($($("#col3")).children());
+var $col4 = jQuery.makeArray($($("#col4")).children());
+var $col5 = jQuery.makeArray($($("#col5")).children());
+var $col6 = jQuery.makeArray($($("#col6")).children());
+
+var $cols = [$col0, $col1, $col2, $col3, $col4, $col5, $col6];
+
+var $row0 = jQuery.makeArray($(".row0"));
+var $row1 = jQuery.makeArray($(".row1"));
+var $row2 = jQuery.makeArray($(".row2"));
+var $row3 = jQuery.makeArray($(".row3"));
+var $row4 = jQuery.makeArray($(".row4"));
+var $row5 = jQuery.makeArray($(".row5"));
+
+var $rows = [$row0, $row1, $row2, $row3, $row4, $row5];
+
+var $diag0 = jQuery.makeArray($(".diag0"));
+var $diag1 = jQuery.makeArray($(".diag1"));
+var $diag2 = jQuery.makeArray($(".diag2"));
+var $diag3 = jQuery.makeArray($(".diag3"));
+var $diag4 = jQuery.makeArray($(".diag4"));
+var $diag5 = jQuery.makeArray($(".diag5"));
+var $diag6 = jQuery.makeArray($(".diag6"));
+var $diag7 = jQuery.makeArray($(".diag7"));
+var $diag8 = jQuery.makeArray($(".diag8"));
+var $diag9 = jQuery.makeArray($(".diag9"));
+var $diag10 = jQuery.makeArray($(".diag10"));
+var $diag11 = jQuery.makeArray($(".diag11"));
+
+var $diags = [$diag0, $diag1, $diag2, $diag3, $diag4, $diag5, $diag6, $diag7, $diag8, $diag9, $diag10, $diag11];
 
 var $spots = $(".spot");
 
@@ -115,7 +66,9 @@ var player1 = "red";
 var player2 = "black";
 var totalMoves = 0;
 var spotIndex;
-
+var $buttons = $("button");
+var $findColumn;
+var $currentColumn;
 
 // ===========================
 // 					Functions
@@ -127,6 +80,19 @@ if (totalMoves % 2 === 0) {
 } else {
 	player = player2;
 }
+
+var checkLowestSpot = function() {
+	$findColumn = $(this).siblings()[0];
+	$currentColumn = jQuery.makeArray($(findColumn).children());
+
+	for (var i = ($currentColumn.length - 1); i >= 0; i--) {
+		if (!($($currentColumn[i]).hasClass("red")) && !($($currentColumn[i]).hasClass("black"))) {
+			$($currentColumn).addClass(player);
+		}
+	}
+
+}
+
 
 
 var setSpotColor = function() {
@@ -153,6 +119,14 @@ var iterating = function(array) {
 for (var i = 0; i < $spots.length; i++) {
 	$($spots[i]).click(setSpotColor);
 	$($spots[i]).click(setSpotIndex);
+}
+
+
+// $($buttons[0]).siblings().children();
+
+
+for (var i = 0; i < $buttons.length; i++) {
+	$($buttons[i]).click(checkLowestSpot);
 }
 
 
