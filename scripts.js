4adcce85ee39ab2076f098077691e14e22ced2bb
Katie Zhou <katiezhou@Katies-MBP.zillionz.net>
2015-10-18 20:02:01 -0400

    adding input to specify board dimensions

--- scripts.js
+++ scripts.js
@@ -3,63 +3,14 @@
 // ===================================================================================
 
 // ==============================================
-// 			Global Variables + Populating Arrays
+// 								Global Variables
 // ==============================================
-
-// here I'm creating one multidimensional array each for rows, columns, and diagonals  
-// with more than 4 spaces to simplify the function that checks for a win later on. 
-// These variables use jQuery to grab elements with the corresponding classes from the HTML doc.
-// var $columns = $(".column");
-
-// var $col0 = jQuery.makeArray($($("#col0")).children());
-// var $col1 = jQuery.makeArray($($("#col1")).children());
-// var $col2 = jQuery.makeArray($($("#col2")).children());
-// var $col3 = jQuery.makeArray($($("#col3")).children());
-// var $col4 = jQuery.makeArray($($("#col4")).children());
-// var $col5 = jQuery.makeArray($($("#col5")).children());
-// var $col6 = jQuery.makeArray($($("#col6")).children());
-
-// // multidimensional array for columns
-// var $cols = [$col0, $col1, $col2, $col3, $col4, $col5, $col6];
-
-
-// var $row0 = jQuery.makeArray($(".row0"));
-// var $row1 = jQuery.makeArray($(".row1"));
-// var $row2 = jQuery.makeArray($(".row2"));
-// var $row3 = jQuery.makeArray($(".row3"));
-// var $row4 = jQuery.makeArray($(".row4"));
-// var $row5 = jQuery.makeArray($(".row5"));
-
-// // multidimensional array for rows
-// var $rows = [$row0, $row1, $row2, $row3, $row4, $row5];
-
-
-// var $diag0 = jQuery.makeArray($(".diag0"));
-// var $diag1 = jQuery.makeArray($(".diag1"));
-// var $diag2 = jQuery.makeArray($(".diag2"));
-// var $diag3 = jQuery.makeArray($(".diag3"));
-// var $diag4 = jQuery.makeArray($(".diag4"));
-// var $diag5 = jQuery.makeArray($(".diag5"));
-// var $diag6 = jQuery.makeArray($(".diag6"));
-// var $diag7 = jQuery.makeArray($(".diag7"));
-// var $diag8 = jQuery.makeArray($(".diag8"));
-// var $diag9 = jQuery.makeArray($(".diag9"));
-// var $diag10 = jQuery.makeArray($(".diag10"));
-// var $diag11 = jQuery.makeArray($(".diag11"));
-
-// // multidimensional array for diagonals
-// var $diags = [$diag0, $diag1, $diag2, $diag3, $diag4, $diag5, $diag6, $diag7, $diag8, $diag9, $diag10, $diag11];
-
-// var $spots = $(".spot");
 
 var rows = [];
 var columns = [];
 
 var player;
 var totalMoves = 0;
-
-// variable used in click event to make a move
-var $spotButtons = $(".spot-button");
 
 // variables used in function that checks for lowest available spot in column and places piece there
 var $findColumn;
@@ -101,6 +52,13 @@ var $player1score = $("#player1-score");
 var $player2score = $("#player2-score");
 var $playAgain = $("#play-again-container");
 
+
+// board size variables
+var $boardSizeContainer = $("#board-size-container");
+var $rowsInput = $("#rows-input");
+var $columnsInput = $("#columns-input");
+var $nextButton = $("#next-button");
+
 // ===========================
 // 					Functions
 // ===========================
@@ -130,13 +88,13 @@ var createBoard = function(columns, rows) {
 	for (var i = 0; i < $spotButtons.length; i++) {
 		$($spotButtons[i]).click(placeLowestSpot);
 	}
-
 	populateArrays();
+
 }
 
 // dynamically creates row and column arrays from HTML elements to allow simpler win checking later
 var populateArrays = function() {
-	var $createColumn = $(".column");
+	$createColumn = $(".column");
 
 	for (var i = 0; i < $createColumn[0].children.length; i++) {
 		rows.push([]);
@@ -156,20 +114,27 @@ var populateArrays = function() {
 
 
 
+var setUp = function() {
+	var $rowsValue = parseInt($rowsInput.val());
+	var $columnsValue = parseInt($columnsInput.val());
+	createBoard($columnsValue, $rowsValue);
+	$($boardSizeContainer).hide();
+	playerNames();
+}
+
 // initial setup function that clears the board and resets all settings changed during gameplay
 // called again in "play again" function
 var initialize = function() {
+	$($boardSizeContainer).show();
 	$($($header).children()[0]).css("font-size", "120px");
 	$($($header).children()[1]).css("font-size", "60px");
-	createBoard(7, 6);
-
 	$($spotContainer).hide();
 	$($instructions).hide();
 	$($playerOrderContainer).hide();
 	$($currentPlayer).hide();
 	$($gameOutcomeContainer).hide();
-	$($nameInputContainer).show();
-	$($startButtonContainer).show();
+	$($nameInputContainer).hide();
+	$($startButtonContainer).hide();
 	$($playAgain).hide();
 	totalMoves = 0;
 	winner = null;
@@ -187,6 +152,12 @@ var initialize = function() {
 	}
 }
 
+var playerNames = function() {
+	$($nameInputContainer).show();
+	$($startButtonContainer).show();
+}
+
+
 
 // I originally just had the "play again" button on a click event to run "initialize" 
 // but I wanted a scoreboard, so now "play again" doesn't ask for player names,
@@ -200,6 +171,7 @@ var playAgainSetup = function() {
 
 // function that randomizes the player order and tells the user(s) who is going first
 var rollDice = function() {
+	$boardSizeContainer.hide();
 	$($playerOrder).html("");
 	$($playerOrderContainer).show();
 	$($diceRollGif).show();
@@ -322,8 +294,8 @@ var rowColumnWinCheck = function(array) {
 // 2 for loops - one for each "direction" of diagonals
 var diagonalWinCheck = function(array) {
 	var $createColumn = $(".column");
-	for (var b = 0; b <= ($createColumn[0].children.length - 3); b++) {
-		for (var c = 0; c <= (6 - 3); c++) {
+	for (var b = 0; b < ($createColumn[0].children.length - 3); b++) {
+		for (var c = 0; c < ($createColumn.length - 3); c++) {
 
 			if ($(array[b][c]).hasClass(player1Color) && $(array[b + 1][c + 1]).hasClass(player1Color) && $(array[b + 2][c + 2]).hasClass(player1Color) && $(array[b + 3][c + 3]).hasClass(player1Color)) {
 				winner = $player1Name;
@@ -336,7 +308,7 @@ var diagonalWinCheck = function(array) {
 			}
 		}
 
-		for (var d = ($createColumn[0].children.length - 1); d > 2; d--) {
+		for (var d = ($createColumn[0].children.length - 1); d >= 3; d--) {
 
 			if ($(array[b][d]).hasClass(player1Color) && $(array[b + 1][d - 1]).hasClass(player1Color) && $(array[b + 2][d - 2]).hasClass(player1Color) && $(array[b + 3][d - 3]).hasClass(player1Color)) {
 				winner = $player1Name;
@@ -393,6 +365,7 @@ var showGameOutcome = function() {
 
 $(document).ready(initialize);
 
+$($nextButton).click(setUp);
 
 $($startButton).click(rollDice);
 
